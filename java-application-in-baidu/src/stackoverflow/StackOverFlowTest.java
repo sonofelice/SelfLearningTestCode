d353668b5aef1ba2bee1706a093b1f494bcaace7
SonoFelice <sonofelice_mm@163.com>
2015-12-18 19:23:36 +0800

    add

---
+++ java-application-in-baidu/src/stackoverflow/StackOverFlowTest.java
@@ -0,0 +1,11 @@
+package stackoverflow;
+
+/**
+ * @author zhangmengmeng01@baidu.com
+ * @version 2015年12月18日 下午7:22:57 类说明 :
+ */
+public class StackOverFlowTest {
+    public static void main(String[] args) {
+        System.out.println("overflow");
+    }
+}

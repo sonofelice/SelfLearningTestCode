92543a3a3a8f21fe83b74441e98071071fa41a6c
SonoFelice <sonofelice_mm@163.com>
2015-12-22 19:48:12 +0800

    edit

--- java-application-in-baidu/src/main/java/com/baidu/mm/algorithom/Test.java
+++ java-application-in-baidu/src/main/java/com/baidu/mm/algorithom/Test.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(3);
-        TreeNode r1 = new TreeNode(1);
-        TreeNode r2 = new TreeNode(2);
-        TreeNode r5 = new TreeNode(5);
-        TreeNode r6 = new TreeNode(6);
-        TreeNode r8 = new TreeNode(8);
-        TreeNode r7 = new TreeNode(7);
+        Node root = new Node(3);
+        Node r1 = new Node(1);
+        Node r2 = new Node(2);
+        Node r5 = new Node(5);
+        Node r6 = new Node(6);
+        Node r8 = new Node(8);
+        Node r7 = new Node(7);
         root.left = r1;
         root.right = r5;
         r1.right = r2;
@@ -23,14 +23,14 @@ public class Test {
 
     }
 
-    public static void allRoad(TreeNode root) {
-        List<TreeNode> list = new LinkedList<TreeNode>();
+    public static void allRoad(Node root) {
+        List<Node> list = new LinkedList<Node>();
         if (root == null) {
             return;
         }
         list.add(root);
         boolean isLeft = true;
-        TreeNode tmpNode = null;
+        Node tmpNode = null;
         while (true) {
             if (root.left != null && isLeft) {
                 list.add(root.left);
@@ -65,10 +65,10 @@ public class Test {
         }
     }
 
-    public static void printRoad(List<TreeNode> list) {
+    public static void printRoad(List<Node> list) {
         String str = "";
         for (int i = 0; i < list.size(); i++) {
-            str = str + list.get(i).val + " ";
+            str = str + list.get(i).value + " ";
         }
         System.out.println(str);
     }
